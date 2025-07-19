6b1960ecf24e9548885004fbc0a50dc3051fbc20
David James <davidanthoneyjames@gmail.com>
2025-07-19 16:22:15 -0400

    This is the completed object and array iteration lab

--- test/helpers.js
+++ test/helpers.js
@@ -1,36 +1,33 @@
-const chai = require('chai')
-const sinon = require('sinon')
-global.expect = chai.expect
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
+const chai = require('chai');
+const sinon = require('sinon');
+global.expect = chai.expect;
+const fs = require('fs');
+const path = require('path');
+const babel = require('@babel/core');
 const assert = require('assert');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
 
 // Use Babel to transform the code
 const babelResult = babel.transform(js, {
-    presets: ['env']
+    presets: ['@babel/preset-env']
 });
 
-const src = babelResult.code
+const src = babelResult.code;
 
-jsdom({
-    html, src
-});
+const vm = require('node:vm');
+vm.runInThisContext(src, {filename: 'index.js'});
 
 module.exports = {
     assert,
     chai,
     sinon,
     fs,
-    jsdom,
     path,
     babel,
     html,
     babelResult,
     src,
     js
-}
+};
